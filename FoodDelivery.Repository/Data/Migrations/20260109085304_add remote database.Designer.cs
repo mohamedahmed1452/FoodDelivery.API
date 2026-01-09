63c6942c094b7215ce04d0aea93d1d9dedcc7404
Mohamed Ahmed <20201700656@cis.asu.edu.ed>
2026-01-09 11:41:03 +0200

    done

--- FoodDelivery.Repository/Data/Migrations/20260109085304_add remote database.Designer.cs
+++ FoodDelivery.Repository/Data/Migrations/20260109085304_add remote database.Designer.cs
@@ -4,6 +4,7 @@ using FoodDelivery.Repository.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FoodDelivery.Repository.Data.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    partial class StoreContextModelSnapshot : ModelSnapshot
+    [Migration("20260109085304_add remote database")]
+    partial class addremotedatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
